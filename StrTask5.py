5fea8d1239068c86d14cd6ae420264f56861ccc5
andrejmoroz89 <99413529+andrejmoroz89@users.noreply.github.com>
2022-03-22 16:56:29 +0300

    Add files via upload

---
+++ StrTask5.py
@@ -0,0 +1,11 @@
+
+STROKA = 'HELLO, my name is Andrey'
+print(STROKA[2])
+print(STROKA[-2])
+print(STROKA[0:5])
+print(STROKA[:-2])
+print(STROKA[0::2])
+print(STROKA[1::2])
+print(STROKA[::-1])
+print(STROKA[-1::-2])
+print(len(STROKA))
